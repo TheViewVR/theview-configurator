95f824cd7b349ba2fe2fae61f87bb4df2c0630e3
irina <iryna@theviewpro.com>
2023-12-15 11:04:55 +0300

    feat: added palette, theme and fonts

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+
+const App: FC = () => (
+  <div className='App'>
+    <header className='App-header'>Configurator</header>
+  </div>
+);
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,18 @@
+import { StrictMode } from 'react';
+import ReactDOM from 'react-dom/client';
+
+import './index.less';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement,
+);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
+
+reportWebVitals();
