b69329773d9278aad91b4b5a14ee8709f461a142
irina <iryna@theviewpro.com>
2023-12-29 17:14:34 +0300

    fix: fixed anchors behavior

--- src/components/sections/AddOnsSection/styled.tsx
+++ src/components/sections/AddOnsSection/styled.tsx
@@ -5,4 +5,7 @@ import { StyledSectionWrapper } from '../ContentSection/styled';
 export const SectionWrapper = styled(StyledSectionWrapper)({
   marginTop: '40px',
   fontFamily: 'Poppins, sans-serif',
+  '@media (min-width: 1920px)': {
+    marginBottom: '120px',
+  },
 });
