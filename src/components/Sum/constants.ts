edac8c9e1796cad82c2af0ec799f49ff40901772
irina <iryna@theviewpro.com>
2024-01-17 17:05:56 +0300

    Updates

--- src/components/Sum/constants.ts
+++
@@ -1,8 +0,0 @@
-export const SUM_TEXT = {
-  FREE: 'free',
-  ONE_TIME: 'one-time',
-  FEE: 'fee',
-  MO: '/mo',
-  MONTH: '/month',
-  HOSTING: 'hosting',
-};
--- src/constants/menu.tsx
+++
@@ -1,2 +0,0 @@
-export const HEADER_HEIGHT = 80;
-export const BASKET_WIDTH = 360;
